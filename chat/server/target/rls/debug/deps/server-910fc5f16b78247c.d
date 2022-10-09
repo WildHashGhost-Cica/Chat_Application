381adabebd1a0a8a95c6bfd3b667f9b1acf20b02
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-10-09 13:50:14 +0100

    server

---
+++ chat/server/target/rls/debug/deps/server-910fc5f16b78247c.d
@@ -0,0 +1,5 @@
+/home/wildhash/projects/chat_app/chat/server/target/rls/debug/deps/server-910fc5f16b78247c.rmeta: src/main.rs
+
+/home/wildhash/projects/chat_app/chat/server/target/rls/debug/deps/server-910fc5f16b78247c.d: src/main.rs
+
+src/main.rs:
