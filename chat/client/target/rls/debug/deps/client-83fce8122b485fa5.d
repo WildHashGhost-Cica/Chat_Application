5065f078a995bae2860efb52ad59954f766b0a8c
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-10-09 13:48:41 +0100

    client

---
+++ chat/client/target/rls/debug/deps/client-83fce8122b485fa5.d
@@ -0,0 +1,5 @@
+/home/wildhash/projects/chat_app/chat/client/target/rls/debug/deps/client-83fce8122b485fa5.rmeta: src/main.rs
+
+/home/wildhash/projects/chat_app/chat/client/target/rls/debug/deps/client-83fce8122b485fa5.d: src/main.rs
+
+src/main.rs:
---
+++ chat/client/target/rls/debug/deps/client-b36e48845d1d5f20.d
@@ -0,0 +1,5 @@
+/home/wildhash/projects/chat_app/chat/client/target/rls/debug/deps/client-b36e48845d1d5f20.rmeta: src/main.rs
+
+/home/wildhash/projects/chat_app/chat/client/target/rls/debug/deps/client-b36e48845d1d5f20.d: src/main.rs
+
+src/main.rs:
